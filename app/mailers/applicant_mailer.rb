5cb24cefbdb0c60c3a64b24efc7c68f56208675e
Olalekan Eyiowuawi <olalekan.eyiowuawi@andela.com>
2017-04-13 14:21:01 +0100

    add mailer specs

--- app/mailers/applicant_mailer.rb
+++ app/mailers/applicant_mailer.rb
@@ -2,7 +2,7 @@ class ApplicantMailer < ApplicationMailer
   default from: 'no-reply@teencode.com'
 
   def welcome_email(id)
-    @user = User.find(id)
-    mail(to: user.email, subject: "Welcome to Teencode")
+    @user = Applicant.find(id)
+    mail(to: @user.email, subject: "Welcome to Teencode")
   end
 end
