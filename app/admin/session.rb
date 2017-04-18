5db4ed37cafede7e3bc75186aa44b382ad674183
andela-gogbara <godwin.onisofien@andela.com>
2017-04-18 07:42:44 +0100

    completed working version active admin

--- app/admin/session.rb
+++ app/admin/session.rb
@@ -1,66 +1,66 @@
 ActiveAdmin.register Session do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-permit_params :title, :description, project_attributes: [:id, :title, :description, :_destroy => true]
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
-# index do
-#   selectable_column
-#   id_column
-#   column :title
-#   column :description
-#   # column :sign_in_count
-#   # column :created_at
-#   actions
-# end
+permit_params(
+  :title,
+  :description,
+  projects_attributes: [:id, :title, :description, :_destroy],
+  resources_attributes: [:id, :link, :description, :_destroy],
+  objectives_attributes: [:id, :description]
+)
 
 show do
-   tabs do
-     tab 'Overview' do
-       attributes_table do
-         row(:title)
-         row(:description)
-       end
-     end
+  attributes_table do
+    row :title
+    row :description
 
-     tab 'Payments' do
-       table_for session.projects do
-         row('Payment Type') { |p| p.title.titleize }
-        #  column('Received On', :created_at)
-        #  column('Payment Details & Notes', :notes)
-        #  column('Amount') { |p| number_to_currency(p.amount_in_dollars) }
-       end
-     end
-   end
- end
+    panel "Session Projects" do
+      table_for session.projects do
+        column :title
+        column :description
+      end
+    end
+
+    panel "Session Resources" do
+      table_for session.resources do
+        column :link
+        column :description
+      end
+    end
+
+    panel "Session Objectives" do
+      table_for session.objectives do
+        column :description
+      end
+    end
+  end
+end
 
 form do |f|
   f.inputs "Curriculum Session" do
     f.input :title, label: "Session Title"
     f.input :description, label: "Session Description"
+  end
 
-    f.fields_for :project do |f|
-      f.input :title, label: 'Project Title'
-      f.input :description, label: 'Project Description'
-    end
-
-    f.fields_for :resource do |f|
-      f.input :link, label: "Resource Link"
-      f.text_area :description, label: 'Resource Description'
-    end
-
-    f.fields_for :objective do |f|
-      f.input :description, label: 'Objective Description'
+  f.inputs do
+    f.has_many :projects, heading: "Session Projects", allow_destroy: true do |p|
+      p.input :title
+      p.input :description
     end
   end
+
+  f.inputs do
+    f.has_many :resources, heading: "Session Resources", allow_destroy: true do |p|
+      p.input :link
+      p.input :description
+    end
+  end
+
+  f.inputs do
+    f.has_many :objectives, heading: "Session Objectives", allow_destroy: true do |p|
+      p.input :description
+    end
+  end
+
   f.actions
 end
 
