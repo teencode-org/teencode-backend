efbd3036028662c5b7e31fe0c192ce08204dcb25
Mayowa Pitan <mayowa.pitan@andela.com>
2017-04-19 12:05:19 +0100

    complete specs to test sessions result functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :applicants
       resources :inquiries
-      resources :sessions, only: [:index, :show]
+      resources :sessions, only: :index
     end
   end
 end
