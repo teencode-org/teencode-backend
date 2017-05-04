c37a8f244e41f11f6a203e28a553ba80c100d41d
Olalekan Eyiowuawi <olalekan.eyiowuawi@andela.com>
2017-05-04 16:10:35 +0100

    remove profession validation of applicant

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,6 +1,5 @@
 class Applicant < ApplicationRecord
   validates :name, presence: true
-  validates :profession, presence: true
   validates :phone_number, presence: true
   validates :email, presence: true
 end
