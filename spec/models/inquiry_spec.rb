53131517111b523fcdaefb92ca02aca1271e92fa
Olalekan Eyiowuawi <olalekan.eyiowuawi@andela.com>
2017-05-12 17:48:37 +0100

    remove phone number validation on inquiry model. Also add name validation test on inquiry model (#15)

--- spec/models/inquiry_spec.rb
+++ spec/models/inquiry_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Inquiry, type: :model do
   end
 
   describe 'model validations' do
-    it { should validate_presence_of :phone_number }
+    it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should validate_presence_of :message }
   end
