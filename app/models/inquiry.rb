43cbb400fc671beb638d80fccf467c6eea964f5d
Olalekan Eyiowuawi <olalekan.eyiowuawi@andela.com>
2017-05-12 16:44:21 +0100

    remove phone number validation on inquiry model. Also add name validation test on inquiry model

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,10 +1,9 @@
 class Inquiry < ApplicationRecord
   VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\.]+[\w+]\.[a-z]+\z/i
-  validates :phone_number,:message, :name,  presence: true
+  validates :message, :name, presence: true
   validates(
     :email,
     presence: true,
     format: { with: VALID_EMAIL }
   )
-
 end
