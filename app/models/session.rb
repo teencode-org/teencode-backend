9e2f28ed0df46e0ea8970f3a6dde08fc76cb02ed
andela-gogbara <godwin.onisofien@andela.com>
2017-05-21 13:33:49 +0100

    fix dependent destroy issue

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,7 +1,7 @@
 class Session < ApplicationRecord
-  has_one :project, inverse_of: :session
-  has_one :resource, inverse_of: :session
-  has_one :objective, inverse_of: :session
+  has_one :project, inverse_of: :session, dependent: :destroy
+  has_one :resource, inverse_of: :session, dependent: :destroy
+  has_one :objective, inverse_of: :session, dependent: :destroy
 
   accepts_nested_attributes_for :project
   accepts_nested_attributes_for :resource
