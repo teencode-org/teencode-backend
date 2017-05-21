48db552ccb5d301f8e08709fc230ecab7d4575ce
andela-gogbara <godwin.onisofien@andela.com>
2017-05-21 11:39:31 +0100

    fix bug

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,13 +2,8 @@ class Session < ApplicationRecord
   has_one :project, inverse_of: :session
   has_one :resource, inverse_of: :session
   has_one :objective, inverse_of: :session
-  before_save :tets
 
   accepts_nested_attributes_for :project
   accepts_nested_attributes_for :resource
   accepts_nested_attributes_for :objective
-
-  def tets
-    # binding.pry
-  end
 end
