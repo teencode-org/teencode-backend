051e2df32bdc2042673d1ecdce7c2602fb005719
Mayowa Pitan <mayowa.pitan@andela.com>
2017-07-07 13:23:56 +0100

    WIP fix textareas and tinymce wysiwig

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170703101833) do
+ActiveRecord::Schema.define(version: 20170703110422) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -125,4 +125,5 @@ ActiveRecord::Schema.define(version: 20170703101833) do
     t.datetime "updated_at",  null: false
     t.integer  "order"
   end
+
 end
