aa788cb93b2e55b978616d1d8cc7abd1327138f0
Mayowa Pitan <andela-mpitan@users.noreply.github.com>
2017-07-19 21:28:41 +0100

    return empty array if blogs are empty (#25)

--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,5 +1,5 @@
 class BlogSerializer < ActiveModel::Serializer
-  attributes :id, :title, :story
+  attributes :id, :title, :story, :featured_image_url
 
   has_one :author, serializer: AuthorSerializer
 end
