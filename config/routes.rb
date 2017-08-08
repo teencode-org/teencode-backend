cacdbaf052a7c7e4e6c8cd19b408650ca78fd5c2
Mayowa Pitan <andela-mpitan@users.noreply.github.com>
2017-08-08 13:24:40 +0100

    Add featured to blogs (#27)
    * return empty array if blogs are empty
    
    * fix params error
    
    * extract featured into its own route
    
    * fix hound violations

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get "blogs/featured", to: "blogs#featured_blogs"
+
       resources :applicants
       resources :inquiries
       resources :sessions, path: :curriculum_sessions, only: :index
