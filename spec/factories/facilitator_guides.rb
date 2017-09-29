45afa3e6803c57d09415ce5d34df03da2aebd25d
Mayowa Pitan <mayowa.pitan@andela.com>
2017-09-29 17:10:29 +0100

    nest the facilitator-guide resource under sessions

--- spec/factories/facilitator_guides.rb
+++ spec/factories/facilitator_guides.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :facilitator_guide do
+    session
     title { Faker::Lorem.word }
     level { Faker::Lorem.word }
-    session { Faker::Number.between(1, 10) }
     intro_video "MyString"
     body { Faker::Lorem.paragraph(2) }
   end
