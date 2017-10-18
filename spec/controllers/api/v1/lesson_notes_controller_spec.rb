4a05ac56740216c352cc03d827dfe2c69ac9ed13
Mayowa Pitan <mayowa.pitan@andela.com>
2017-10-18 19:33:08 +0100

    update lesson notes controller spec

--- spec/controllers/api/v1/lesson_notes_controller_spec.rb
+++ spec/controllers/api/v1/lesson_notes_controller_spec.rb
@@ -2,35 +2,53 @@ require "rails_helper"
 
 RSpec.describe Api::V1::LessonNotesController, type: :controller do
   describe "#show" do
-    context "when a valid lesson note id is sent" do
-      it "should return a successful response" do
-        guide = create(:lesson_note)
+    context "when valid sessions are passed" do
+      context "when a valid lesson note id is sent" do
+        it "should return a successful response" do
+          note = create(:lesson_note)
 
-        get :show, params: { id: guide.id }
+          get :show, params: { session_id: note.session.id, id: note.id }
 
-        expect(response).to have_http_status(200)
+          expect(response).to have_http_status(200)
+        end
+
+        it "should return the lesson note as well as the author" do
+          note = create(:lesson_note, authors: create_list(:author, 2))
+
+          get :show, params: { session_id: note.session.id, id: note.id }
+
+          expect(json(response.body)[:authors]).to be_present
+        end
       end
 
-      it "should return the lesson note as well as the author" do
-        guide = create(:lesson_note, authors: create_list(:author, 2))
+      context "when an invalid lesson note is requested" do
+        let(:session) { session = create(:session) }
 
-        get :show, params: { id: guide.id }
+        it "should return without a body" do
+          get :show, params: { session_id: session.id, id: "fake id" }
 
-        expect(json(response.body)[:authors]).to be_present
+          expect(response.body).to be_empty
+        end
+
+        it "should return a not found status" do
+          get :show, params: { session_id: session.id, id: "fake id" }
+
+          expect(response).to have_http_status(404)
+        end
       end
     end
 
-    context "when an invalid lesson note is requested" do
-      it "should return without a body" do
-        get :show, params: { id: "fake id" }
-
-        expect(response.body).to be_empty
+    context "when an invalid session is passed" do
+      before do
+        get :show, params: { session_id: "fake id", id: "anything" }
       end
 
-      it "should return a not found status" do
-        get :show, params: { id: "fake id" }
+      it "should return a status of 403" do
+        expect(response).to have_http_status(403)
+      end
 
-        expect(response).to have_http_status(404)
+      it "should return a meaningful message" do
+        expect(json(response.body)[:error]).to eql "Invalid session id"
       end
     end
   end
