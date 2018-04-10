065348a5cbb1c5f3ed59173f5ab9c33bc76f1945
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-04-10 10:10:31 +0100

    - add model tests

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe School, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many(:reports) }
+  it { is_expected.to belong_to(:facilitator) }
 end
